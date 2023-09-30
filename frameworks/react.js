13fea444eb95942128851aa1dc13961889d8dfb0
Mikael Hadler <mhadler@lendingclub.com>
2023-09-30 13:01:44 -0300

    up files to react structure

--- frameworks/react.js
+++ frameworks/react.js
@@ -1,8 +1,17 @@
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+// const React = require('react');
+const { createRoot } = require('react-dom/client');
 
 function App() {
+  console.log('pass here');
   return <h1>A simple handle with react</h1>;
 }
+
+// class Greeting extends React.Component {
+//   render() {
+//     return <h1>Hello</h1>;
+//   }
+// }
 
 const domNode = document.getElementById('app');
 const root = createRoot(domNode);
